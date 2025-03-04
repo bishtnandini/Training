ddc8128c836f3b90a332980366c7410c7e3c3dbf
bishtnandini <nandinibisht005@gmail.com>
2025-03-05 02:21:08 +0530

    collection-generic

---
+++ Collection-Generic/Collection-Generic/PHashTable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Generic
+{
+    public class PHashTable
+    {
+        //similar to a Dictionary, but it is not type-safe since it stores objects.
+        public static void Run()
+        {
+            Hashtable ht = new Hashtable();
+
+            //add
+            ht.Add(1, "hi");
+            ht.Add(2, "hello");
+            ht.Add(3, "hey");
+
+            //remove
+            ht.Remove(2);
+
+            //contain
+            if (ht.ContainsKey(3))
+            {
+                Console.WriteLine("Key 3 exists in the Hashtable.");
+            }
+            else
+            {
+                Console.WriteLine("Key 3 does not exist.");
+            }
+
+            //clone
+            Hashtable htt = (Hashtable)ht.Clone();
+            htt.Add(4, "yo");
+            foreach (DictionaryEntry item in htt)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+
+          //keys
+            foreach (var key in htt.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            //values
+            foreach (var val in htt.Values)
+            {
+                Console.WriteLine(val);
+            }
+
+            //both
+
+            foreach (DictionaryEntry i in htt)
+            {
+                Console.WriteLine($"key is {i.Key} , value is {i.Value}");
+            }
+
+
+
+        }
+    }
+}
---
+++ Collection-Generic/Collection-Generic/PList.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection_Generic
+{
+    public class PList
+    {
+        //similar to arraylist but more type secure, better performance
+        public static void Run()
+        {
+            List<int> li = new List<int>();
+
+            // Add elements to the integer list
+            li.Add(1);
+            li.Add(2);
+
+            // List of Customer objects 
+            List<Customer> cus = new List<Customer>();
+
+            //if name is property
+            //cus.Add(new Customer { name = "Nandini" });
+
+            //if name is variable field 
+            cus.Add(new Customer("Nandini"));
+
+            // Display customers
+            foreach (var c in cus)
+            {
+                Console.WriteLine("Customer Name: " + c.name);
+            }
+
+            var states = new[]
+            {
+              "Uttarakhand",
+              "A&N",
+              "J&K"
+            };
+
+            List<string> st = new List<string>(states);
+            st.Add("himachal pradesh");
+
+            st.ForEach(x => Console.WriteLine(x));
+
+            //addrange (copying list)
+            List<string> newst = new List<string>();
+            newst.Add("Punjab");
+            newst.AddRange(st);
+            foreach (var c in newst)
+            {
+                Console.WriteLine( c);
+            }
+
+            //insert
+            newst.Insert(1, "Washington");
+
+            //remove
+            newst.Remove("Washington");
+            //removaALL
+            newst.RemoveAll( x => x == "Washington");
+
+            //contains
+            Console.WriteLine(newst.Contains("Uttarakhand"));
+
+
+            //indexof
+            Console.WriteLine(newst.IndexOf("Uttarakhand"));
+
+            //find
+            Console.WriteLine(newst.Find(x=>x.Contains("&")));
+
+            //findALL
+          var result=newst.FindAll(x => x.Contains("&"));
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);                
+            }
+        }
+    }
+
+    public class Customer
+    {
+       // public string name { get; set; }
+        
+        public string name;
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+        
+    }
+}
