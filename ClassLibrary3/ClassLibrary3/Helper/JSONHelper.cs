1550e387c96d92402978a3a1ceaccf15cf2ffb16
bishtnandini <nandinibisht005@gmail.com>
2025-03-11 01:34:29 +0530

    secure/unsecure config implementation

---
+++ ClassLibrary3/ClassLibrary3/Helper/JSONHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3.Helper
+{
+    public static class JSONHelper
+    {
+        public static T Deserialize<T>(string json)
+        {
+            using(var memoryStream= new MemoryStream(Encoding.Unicode.GetBytes(json)))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                return (T)serializer.ReadObject(memoryStream);
+            }
+        }
+    }
+}
